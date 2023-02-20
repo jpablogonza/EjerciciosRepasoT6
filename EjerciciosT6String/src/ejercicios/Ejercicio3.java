e3ab9390f282e30e1e7f60ce373528d1fda3b891
jpgonzalez <jpgonzalez@106-11.info2>
2023-02-20 13:00:28 +0100

    t6

--- EjerciciosT6String/src/ejercicios/Ejercicio3.java
+++ EjerciciosT6String/src/ejercicios/Ejercicio3.java
@@ -8,21 +8,24 @@ public class Ejercicio3 {
 		
 		//Introducimos las variables
 		String cad1;
-		int contador;
+		int contador = 0;
 		
 		//Abrimos el scanner
 		Scanner sc = new Scanner(System.in);
 		
 		//Le pedimos al usuario que nos de alguna frase
 		System.out.println("Dime lo que quieras: ");
-		cad1 = sc.next();
+		cad1 = sc.nextLine();
 		
 		for (int i = 0; i < cad1.length(); i++) {
 			if (cad1.charAt(i) == ' ') {
-				
+				contador++;
 			}
 		}
 
+		System.out.println("En la frase introducida hay un total de " + contador + " espacios.");
+		
+		sc.close();
 	}
 
 }
